a3c6b2fbebd0a033a009edb8f3ace7bdb486ebdb
Keith Boice <69816300+keithboice@users.noreply.github.com>
2021-02-14 06:58:00 -0500

    rush update

--- packages/lint/conf/prettier.config.js
+++ packages/lint/conf/prettier.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-	useTabs:                 true,
-	tabWidth:                4,
-	semi:                    true,
-	singleQuote:             false,
-	quoteProps:              "consistent",
+	printWidth: 110,
+	endOfLine: "auto",
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "consistent",
 	vueIndentScriptAndStyle: true,
-	jsxSingleQuote:          false
+	jsxSingleQuote: false,
 };
